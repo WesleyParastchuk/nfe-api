2955c3751d0e7013ee826c96ca0955af01f4b482
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-09 17:33:32 -0300

    feat: santa catarina extractor strategy

---
+++ src/service/extraction/santaCatarina/SantaCatarinaNFeStrategy.ts
@@ -0,0 +1,40 @@
+import { GeneralInfoDTO } from "../../../dto/GeneralInfoDTO";
+import { NFeExtractionStrategy } from "../../nfeServices/NFeExtractionStrategy.interface";
+import { IssuerDTO } from "../../../dto/IssuerDTO";
+import { ProductDTO } from "../../../dto/ProductDTO";
+import { SummaryDTO } from "../../../dto/SummaryDTO";
+import { TaxDTO } from "../../../dto/TaxDTO";
+import { CheerioAPI } from "../common/HttpParser";
+import { extractSantaCatarinaGeneralInfo } from "./extractors/GeneralInfoExtractor";
+import { extractSantaCatarinaIssuer } from "./extractors/IssuerExtractor";
+import { extractSantaCatarinaProducts } from "./extractors/ProductExtractor";
+import { extractSantaCatarinaSummary } from "./extractors/SummaryExtractor";
+import { extractSantaCatarinaTaxes } from "./extractors/TaxExtractor";
+
+export class SantaCatarinaNFeStrategy implements NFeExtractionStrategy {
+  private readonly baseUrl = "sat.sef.sc.gov.br";
+
+  matches(url: String): boolean {
+    return url.includes(this.baseUrl);
+  }
+
+  extractGeneralInfo(html: CheerioAPI): GeneralInfoDTO {
+    return extractSantaCatarinaGeneralInfo(html);
+  }
+
+  extractIssuer(html: CheerioAPI): IssuerDTO {
+    return extractSantaCatarinaIssuer(html);
+  }
+
+  extractProducts(html: CheerioAPI): ProductDTO[] {
+    return extractSantaCatarinaProducts(html);
+  }
+
+  extractSummary(html: CheerioAPI): SummaryDTO {
+    return extractSantaCatarinaSummary(html);
+  }
+
+  extractTaxes(html: CheerioAPI): TaxDTO {
+    return extractSantaCatarinaTaxes(html);
+  }
+}
--- src/service/nfeServices/NFeStrategyResolver.ts
+++ src/service/nfeServices/NFeStrategyResolver.ts
@@ -4,9 +4,10 @@ import { DefaultNFeOrchestrator } from "./DefaultNfeExtractorOrchestrator";
 import { DefaultHtmlFetcher } from "./DefaultHtmlFetcher";
 import { DefaultNFeAggregator } from "./DefaultNFeAggregator";
 import { SaoPauloNFeStrategy } from "../extraction/saoPaulo/SaoPauloNFeStrategy";
+import { SantaCatarinaNFeStrategy } from "../extraction/santaCatarina/SantaCatarinaNFeStrategy";
 
 export class NFeStrategyResolver {
-  private strategies: NFeExtractionStrategy[] = [new ParanaNFeStrategy(), new SaoPauloNFeStrategy()];
+  private strategies: NFeExtractionStrategy[] = [new ParanaNFeStrategy(), new SaoPauloNFeStrategy(), new SantaCatarinaNFeStrategy()];
 
   async execute(url: string): Promise<any> {
     const strategy = this.strategies.find((s) => s.matches(url));
